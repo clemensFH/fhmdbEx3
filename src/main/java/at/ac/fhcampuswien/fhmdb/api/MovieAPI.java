f49aed0e16067ce45522681ecab809b63263738a
stormfire-ll <114418651+stormfire-ll@users.noreply.github.com>
2023-06-11 23:14:52 +0200

    BuilderPattern in Arbeit

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -13,62 +13,29 @@ import java.util.List;
 import java.util.UUID;
 
 public class MovieAPI {
+    //public static final String DELIMITER = "&";
     private static final String URL = "http://prog2.fh-campuswien.ac.at/movies"; // https if certificates work
     private static final OkHttpClient client = new OkHttpClient();
 
-    public static List<Movie> getAllMovies() throws MovieApiException {
-        return getAllMovies(null, null, null, null);
-    }
-    public static List<Movie> getAllMovies(String query, Genre genre, String releaseYear, String ratingFrom) throws MovieApiException{
-        String urlAllQueries = new MovieAPIRequestBuilder(URL)
-                .query(query)
-                .genre(genre.toString())
-                .releaseYear(releaseYear)
-                .ratingForm(ratingFrom)
-                .build();
-        Request request = new Request.Builder()
-                .url(urlAllQueries)
-                .removeHeader("User-Agent")
-                .addHeader("User-Agent", "http.agent")  // needed for the server to accept the request
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            String responseBody = response.body().string();
-            Gson gson = new Gson();
-            Movie[] movies = gson.fromJson(responseBody, Movie[].class);
-            return Arrays.asList(movies);
-        } catch (Exception e) {
-            throw new MovieApiException("MovieApiException: Error requesting movie from API. Please check your internet connection and try again later.\n");
-        }
-    }
-    public Movie requestMovieById(UUID id) throws MovieApiException{
-        String urlID = new MovieAPIRequestBuilder(URL + "/movies" + id).build();
-        Request request = new Request.Builder()
-                .url(urlID)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            Gson gson = new Gson();
-            return gson.fromJson(response.body().string(), Movie.class);
-        } catch (Exception e) {
-            throw new MovieApiException("MovieApiException: Error requesting movie from API. Please check your internet connection and try again later.\n");
-        }
-    }
-}
-/*  Ursprünglich
-    private String build(UUID id) { //build ID URL
+    /*
+        private String buildUrl(UUID id) {
         StringBuilder url = new StringBuilder(URL);
         if (id != null) {
             url.append("/").append(id);
         }
         return url.toString();
     }
-
-    private static String buildUrl(String query, Genre genre, String releaseYear, String ratingFrom) { //build specificRequest-URL
+    private static String buildUrl(String query, Genre genre, String releaseYear, String ratingFrom) {
         StringBuilder url = new StringBuilder(URL);
-        if ((query != null && !query.isEmpty()) ||          //if anyone is filled
+
+        if ((query != null && !query.isEmpty()) ||
                 genre != null || releaseYear != null || ratingFrom != null) {
+
             url.append("?");
-            if (query != null && !query.isEmpty()) {        //check all parameters and add them to the url
-                url.append("query=").append(query).append(DELIMITER); //query="Wort"&
+
+            // check all parameters and add them to the url
+            if (query != null && !query.isEmpty()) {
+                url.append("query=").append(query).append(DELIMITER);
             }
             if (genre != null) {
                 url.append("genre=").append(genre).append(DELIMITER);
@@ -82,28 +49,61 @@ public class MovieAPI {
         }
         return url.toString();
     }
+     */
 
+
+    public static List<Movie> getAllMovies() throws MovieApiException {
+        return getAllMovies(null, null, null, null);
+    }
     public static List<Movie> getAllMovies(String query, Genre genre, String releaseYear, String ratingFrom) throws MovieApiException{
+        /*
         String url = buildUrl(query, genre, releaseYear, ratingFrom);
+        */
+        String url = new MovieAPIRequestBuilder(URL)
+                .query(query)
+                .genre(genre.toString())
+                .releaseYear(releaseYear)
+                .ratingForm(ratingFrom)
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
                 .removeHeader("User-Agent")
                 .addHeader("User-Agent", "http.agent")  // needed for the server to accept the request
                 .build();
+
         try (Response response = client.newCall(request).execute()) {
             String responseBody = response.body().string();
             Gson gson = new Gson();
             Movie[] movies = gson.fromJson(responseBody, Movie[].class);
+
             return Arrays.asList(movies);
         } catch (Exception e) {
             throw new MovieApiException("MovieApiException: Error requesting movie from API. Please check your internet connection and try again later.\n");
         }
+        //return new ArrayList<>();
+    }
+    /*
 
+    public Movie requestMovieById(UUID id) throws MovieApiException{
+        String url = buildUrl(id);
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            Gson gson = new Gson();
+            return gson.fromJson(response.body().string(), Movie.class);
+        } catch (Exception e) {
+            throw new MovieApiException("MovieApiException: Error requesting movie from API. Please check your internet connection and try again later.\n");
+        }
+    }
     public Movie getMovieById(String id) throws MovieApiException {
         String url = buildUrl(UUID.fromString(id));
         Request request = new Request.Builder()
                 .url(url)
                 .build();
+
         try (Response response = client.newCall(request).execute()) {
             Gson gson = new Gson();
             return gson.fromJson(response.body().string(), Movie.class);
@@ -112,6 +112,5 @@ public class MovieAPI {
         }
     }
 
-    }
-
- */
+    */
+}
--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPIRequestBuilder.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPIRequestBuilder.java
@@ -2,17 +2,9 @@ package at.ac.fhcampuswien.fhmdb.api;
 
 public class MovieAPIRequestBuilder {
     public static final String DELIMITER = "&";
-    private static final String URL = "http://prog2.fh-campuswien.ac.at/movies"; // https if certificates work
-
-    private String query;
-    private String genre;
-    private String releaseYear;    //private int releaseYear;
-    private String ratingForm;    //private double ratingForm;
-    private String id;
-
     private StringBuilder url;
 
-    public MovieAPIRequestBuilder(String url) {
+    public MovieAPIRequestBuilder(String url) { //? never used
         this.url = new StringBuilder();
     }
 
@@ -57,111 +49,15 @@ public class MovieAPIRequestBuilder {
         return this;
     }
     public MovieAPIRequestBuilder id(String id){
-        if(!questionmarkSet()){
-            url.append("?");
-        }
-        this.id = id;
-        return this;
-    }
-
-    public String build() {
-        return url.toString();
-    }
-}
-
-    /*
-        public MovieAPIRequestBuilder queryStart(String query, Genre genre, String releaseYear, String ratingFrom) {
-        if ((query != null && !query.isEmpty()) ||
-                genre != null || releaseYear != null || ratingFrom != null) {  //if anyone is filled
-            url.append(QUERY_START);
-        }
-        return this;
-    }
-    
-    public MovieAPIRequestBuilder id(UUID id){
         if (id != null) {
+            if(!questionmarkSet()){
+                url.append("?");
+            }
             url.append("id=").append(id).append(DELIMITER);
         }
         return this;
     }
-    */
-
-/*  //Tutorium
-    public MovieAPIRequestBuilder query(String query){
-        this.query = query;
-        return this;
-    }
-    public MovieAPIRequestBuilder genre(String genre){
-        this.genre = genre;
-        return this;
-    }
-    public MovieAPIRequestBuilder releaseYear(String releaseYear){
-        this.releaseYear = releaseYear;
-        return this;
-    }
-    public MovieAPIRequestBuilder ratingForm(String ratingForm){
-        this.ratingForm = ratingForm;
-        return this;
-    }
-
-    //Setter
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public void setReleaseYear(String releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public void setRatingForm(String ratingForm) {
-        this.ratingForm = ratingForm;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    //Leon
-    public String build(String query, Genre genre, String releaseYear, String ratingFrom, UUID id) {
-        StringBuilder url = new StringBuilder(URL);
-
-        if ((query != null && !query.isEmpty()) ||          //if anyone is filled
-                genre != null || releaseYear != null || ratingFrom != null) {
-            url.append("?");
-
-            if (query != null && !query.isEmpty()) {        //check all parameters and add them to the url
-                url.append("query=").append(query).append(DELIMITER); //query="Wort"&
-            }
-            if (genre != null) {
-                url.append("genre=").append(genre).append(DELIMITER);
-            }
-            if (releaseYear != null) {
-                url.append("releaseYear=").append(releaseYear).append(DELIMITER);
-            }
-            if (ratingFrom != null) {
-                url.append("ratingFrom=").append(ratingFrom).append(DELIMITER);
-            }
-            if (id != null) {
-                url.append("id=").append(id).append(DELIMITER);
-            }
-        }
+    public String build() {
         return url.toString();
     }
- */
-/*
-private static boolean filled;
-    //public static final String QUERY_START = "?";
-            public static boolean queryStart(String query, Genre genre, String releaseYear, String ratingFrom) {
-        if ((query != null && !query.isEmpty()) ||
-                genre != null || releaseYear != null || ratingFrom != null) {  //if anyone is filled
-            filled = true;
-        } else {
-            filled = false;
-        }
-        return filled;
-    }
- */
+}
